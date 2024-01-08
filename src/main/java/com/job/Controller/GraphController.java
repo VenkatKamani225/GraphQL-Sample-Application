3581a47cdbe7017aff417df277e5191fe5700387
Venkat Kamani <venkatkamani2250@gmail.com>
2024-01-08 16:55:23 -0500

    added one more Query = findOne()

--- src/main/java/com/job/Controller/GraphController.java
+++ src/main/java/com/job/Controller/GraphController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.HttpStatus;
@@ -25,22 +26,22 @@ public class GraphController {
 		return new ResponseEntity<>(graphService.findAll(),HttpStatus.OK);
 	}
 	@QueryMapping
-	public Optional<Players> findOne(int id) {
+	public Optional<Players> findOne(@Argument int id) {
 		return graphService.findOne(id);
 	}
 	
 	@MutationMapping
-	public void addPlayer(String name, Teams team) {
+	public void addPlayer(@Argument String name, @Argument Teams team) {
 		graphService.addPlayer(name, team);
 	}
 	
 	@MutationMapping
-	public void deletePlayer(int id) {
+	public void deletePlayer(@Argument int id) {
 		graphService.deletePlayer(id);
 	}
 	
 	@MutationMapping
-	public Players updatePlayer(int id, String name, Teams team) {
+	public Players updatePlayer(@Argument int id, @Argument String name, @Argument Teams team) {
 		return graphService.updatePlayer(id, name, team);
 	}
 }
